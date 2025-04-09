7aaf5b36f02ff6e9472a97650869881f0a162255
alexnjoya <njoyaalexander71@gmail.com>
2025-04-09 13:02:41 +0000

    blog

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.app.goo.gl'], // Allow this domain for external images
+    domains: ['images.app.goo.gl', 'th.bing.com'], // Allow this domain for external images
   },
 };
 
